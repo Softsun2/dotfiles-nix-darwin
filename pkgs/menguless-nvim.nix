01dd47fd2c8aa95a311153c006e6933cc003168e
softsun2 <peyton.okubo13@gmail.com>
2023-02-28 12:13:08 -0600

    cope

--- pkgs/menguless-nvim.nix
+++ pkgs/menguless-nvim.nix
@@ -6,7 +6,7 @@ pkgs.vimUtils.buildVimPlugin {
     owner = "zekzekus";
     repo = "menguless";
     rev = "09072d8edb8f94cc1be63ae2c7ff4ae091189f16";
-    sha256 = "sha256-0000000000000000000000000000000000000000000";
+    sha256 = "sha256-2QfSdtq7Jt3Bhx/6iTfltEFrLyl63DZ+RmY0hM8f4Os";
   };
   meta.homepage = "https://github.com/zekzekus/menguless";
 }
